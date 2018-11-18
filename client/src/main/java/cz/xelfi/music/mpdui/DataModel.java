5ff3f2bf9ace392b5c0b78d4a6bdca8ae5e358e9
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2018-11-18 20:26:19 +0100

    Can display currently playing song

--- client/src/main/java/cz/xelfi/music/mpdui/DataModel.java
+++ client/src/main/java/cz/xelfi/music/mpdui/DataModel.java
@@ -23,18 +23,29 @@ import net.java.html.json.Property;
 import net.java.html.json.ModelOperation;
 import net.java.html.json.OnPropertyChange;
 import cz.xelfi.music.mpdui.js.PlatformServices;
+import org.bff.javampd.player.PlayerChangeEvent;
+import org.bff.javampd.player.PlayerChangeListener;
 import org.bff.javampd.server.MPD;
+import org.bff.javampd.song.MPDSong;
 /** Model annotation generates class Data with
  * one message property, boolean property and read only words property
  */
 @Model(className = "Data", targetId = "", instance = true, builder = "put", properties = {
     @Property(name = "message", type = String.class),
     @Property(name = "host", type = String.class),
+    @Property(name = "currentSong", type = Song.class),
     @Property(name = "rotating", type = boolean.class)
 })
 final class DataModel {
+    private Listener listener;
     private PlatformServices services;
     private MPD mpd;
+
+    @ModelOperation
+    void updateStatus(Data model) {
+        MPDSong s = mpd(model).getPlayer().getCurrentSong();
+        model.getCurrentSong().read(s);
+    }
 
     @ComputedProperty static java.util.List<String> words(String message) {
         String[] arr = new String[6];
@@ -54,6 +65,7 @@ final class DataModel {
             mpd = new MPD.Builder()
                 .server(model.getHost())
                 .build();
+            mpd.getPlayer().addPlayerChangeListener(listener);
         }
         return mpd;
     }
@@ -90,6 +102,7 @@ final class DataModel {
 
     @ModelOperation
     void initServices(Data model, PlatformServices services) {
+        this.listener = new Listener(model);
         this.services = services;
         String previousMessage = services.getPreferences("message");
         if (previousMessage != null) {
@@ -106,5 +119,60 @@ final class DataModel {
         ui.setMessage("Hello World from HTML and Java!");
         ui.initServices(services);
         ui.applyBindings();
+        ui.updateStatus();
+    }
+
+    @Model(builder = "put", className = "PlayerStatus", properties = {
+        @Property(name = "currentSong", type = Song.class)
+    })
+    static final class Listener implements PlayerChangeListener {
+        final Data model;
+
+        Listener(Data model) {
+            this.model = model;
+        }
+
+        @Override
+        public void playerChanged(PlayerChangeEvent event) {
+            PlayerChangeEvent.Event ev = event.getEvent();
+            System.err.println("ev: " + ev);
+            model.updateStatus();
+        }
+
+    }
+
+    @Model(builder = "put", className = "Song", properties = {
+        @Property(name = "name", type = String.class),
+        @Property(name = "title", type = String.class),
+        @Property(name = "genre", type = String.class),
+        @Property(name = "comment", type = String.class),
+        @Property(name = "file", type = String.class),
+        @Property(name = "year", type = String.class),
+        @Property(name = "discNumber", type = String.class),
+        @Property(name = "artistName", type = String.class),
+        @Property(name = "albumName", type = String.class),
+        @Property(name = "length", type = int.class),
+        @Property(name = "track", type = int.class),
+        @Property(name = "id", type = int.class),
+        @Property(name = "position", type = int.class),
+    })
+    static final class SongCntrl {
+        @ModelOperation
+        static void read(Song model, MPDSong s) {
+            model
+                .putName(s.getName())
+                .putTitle(s.getTitle())
+                .putGenre(s.getGenre())
+                .putComment(s.getComment())
+                .putFile(s.getFile())
+                .putYear(s.getYear())
+                .putDiscNumber(s.getDiscNumber())
+                .putAlbumName(s.getAlbumName())
+                .putArtistName(s.getArtistName())
+                .putLength(s.getLength())
+                .putTrack(s.getTrack())
+                .putId(s.getId())
+                .putPosition(s.getPosition());
+        }
     }
 }
